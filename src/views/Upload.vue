c1fed005223673172d8d00270abfa4efab9414e3
gavinning <gavinning@qq.com>
2020-02-01 16:11:45 +0800

    0.1.0

---
+++ src/views/Upload.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+.file-upload
+	FileUpload(field="img"
+        @crop-success="cropSuccess"
+        @crop-upload-success="cropUploadSuccess"
+        @crop-upload-fail="cropUploadFail"
+        v-model="show"
+		:width="300"
+		:height="300"
+		url="/upload"
+		:params="params"
+		:headers="headers"
+		img-format="png"
+    )
+</template>
+<script>
+import FileUpload from 'vue-image-crop-upload'
+
+export default {
+    components: { FileUpload },
+
+    data() {
+        return {
+			show: true,
+			params: {
+				token: '123456798',
+				name: 'avatar'
+			},
+			headers: {
+				smail: '*_~'
+			},
+			imgDataUrl: ''
+		}
+    },
+
+    methods: {
+        toggleShow() {
+            this.show = !this.show;
+        },
+        /**
+         * crop success
+         *
+         * [param] imgDataUrl
+         * [param] field
+         */
+        cropSuccess(imgDataUrl, field){
+            console.log('-------- crop success --------');
+            this.imgDataUrl = imgDataUrl;
+        },
+        /**
+         * upload success
+         *
+         * [param] jsonData   服务器返回数据，已进行json转码
+         * [param] field
+         */
+        cropUploadSuccess(jsonData, field){
+            console.log('-------- upload success --------');
+            console.log(jsonData);
+            console.log('field: ' + field);
+        },
+        /**
+         * upload fail
+         *
+         * [param] status    server api return error status, like 500
+         * [param] field
+         */
+        cropUploadFail(status, field){
+            console.log('-------- upload fail --------');
+            console.log(status);
+            console.log('field: ' + field);
+        }
+    }
+}
+</script>
+<style lang="stylus">
+.file-upload
+
+    .vue-image-crop-upload
+        position static
+
+    .vicp-wrap
+        // box-shadow none
+
+    .vicp-close
+        display none
+</style>
